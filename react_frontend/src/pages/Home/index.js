2790510a5d4553936b994db3f1505eec9b99a733
turan1302 <printwordex@gmail.com>
2024-08-04 15:42:53 +0300

    Proje sonu

--- react_frontend/src/pages/Home/index.js
+++ react_frontend/src/pages/Home/index.js
@@ -29,6 +29,24 @@ export class Home extends Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if ((this.state.changeWord !== prevState.changeWord && this.state.changeWord !== '') || (this.state.changeWord !== prevState.changeWord && this.state.changeWord === ' ')){
+            if (!this.state.interval){
+                const intervalId = setInterval(()=>{
+                    this.setState((prevState)=>({
+                        timer : prevState.timer -1
+                    }),()=>{
+                        if (this.state.timer<=0){
+                            clearInterval(this.state.interval);
+                            this.setState({interval : null});
+                        }
+                    })
+                },1000);
+                this.setState({interval : intervalId});
+            }
+        }
+    }
+
     handleWord = (event) => {
         let {words, checkWord, trueCount, falseCount} = this.state;
         let getValue = event.target.value;
@@ -69,6 +87,20 @@ export class Home extends Component {
         }
     }
 
+    restart = ()=>{
+        let newWordList = wordList.sort(() => Math.random() - 0.5);
+
+        clearInterval(this.state.interval);
+        this.setState({
+            changeWord : '',
+            trueCount : 0,
+            falseCount : 0,
+            timer : 60,
+            interval : null,
+            checkWord : newWordList[0]
+        })
+    }
+
     render() {
         let {words,changeWord,checkWord,trueCount,falseCount,truePercent,timer} = this.state;
 
@@ -77,7 +109,7 @@ export class Home extends Component {
                 <Container className={"mt-5"}>
                     <Row>
                         <Col md={12}>
-                            {(timer >= 60) ? (
+                            {(timer > 0) ? (
                                 <Card>
                                     <Card.Header>mType</Card.Header>
                                     <Card.Body>
@@ -109,7 +141,7 @@ export class Home extends Component {
                                                 <InputGroup className={"mt-3"}>
                                                     <Button disabled={true} variant={"outline-secondary"}>{timer}
                                                         sn.</Button>
-                                                    <Button variant={"outline-success ml-2"}>Yenile</Button>
+                                                    <Button onClick={()=>this.restart()} variant={"outline-success ml-2"}>Yenile</Button>
                                                 </InputGroup>
                                             </InputGroup>
                                         </Card>
@@ -125,15 +157,15 @@ export class Home extends Component {
                                             <Alert variant={"success"}>
                                                 <h4>Oyun bitti :)</h4>
                                                 <br/>
-                                                <font style={{fontSize: "50px"}}>60 DKS</font>
+                                                <font style={{fontSize: "50px"}}>{trueCount + falseCount} DKS</font>
                                                 <br/>
-                                                Doğruluk Yüzdesi: 80%
+                                                Doğruluk Yüzdesi: {truePercent.toFixed(2)}%
                                                 <br/>
-                                                Doğru Kelime: 50
+                                                Doğru Kelime: {trueCount}
                                                 <br/>
-                                                Yanlış Kelime: 10
+                                                Yanlış Kelime: {falseCount}
                                                 <br/>
-                                                <Button variant={"primary mt-2"}>Yeni Oyun</Button>
+                                                <Button onClick={()=>this.restart()} variant={"primary mt-2"}>Yeni Oyun</Button>
                                             </Alert>
                                         </Card.Text>
                                     </Card.Body>
